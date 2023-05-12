4d2c273f6795634c14bd1f4575a7ef5edce889c7
VITOORRPORTELA <vitorportelasgtau@gmail.com>
2023-05-12 14:12:03 -0300

    enviando o código

---
+++ tesouro.py
@@ -0,0 +1,6 @@
+moedas = int(input())
+marinheiros = int(input())
+marinheiros += 2
+divisao = moedas / marinheiros
+capitao = 2 * divisao
+print(capitao)
---
+++ tutorial.py
@@ -0,0 +1,34 @@
+#mostrar na tela 
+print("mostrado algo na tela")
+
+#pegar uma iformação
+input()
+
+#savlvar uma informação na variavel
+variavel = input()
+
+#transformar a informação em numero
+numero = int(input())
+
+#criar uma lista
+lista = []
+
+# adicionar um variavel na lista
+variavelNova = "batata"
+lista.append(variavelNova)
+
+#checar um iten da lista
+item = lista[0]
+
+#organizar uma lista
+lista = [44,13,72]
+lista.sort()
+#lista = [13,44,72]
+
+#escolher quantos itens tem na lista
+quantidade = int(input())
+listaFinal = [] 
+for rep in range (quantidade):
+    listaFinal.append(int(input()))
+
+    
